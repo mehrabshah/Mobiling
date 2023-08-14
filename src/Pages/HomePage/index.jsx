390bc10529ff61c138a8e3d91ca1155db1e5c071
hassanshahzadse <hassanshahzadvs@gmail.com>
2023-08-15 01:26:20 +0500

    updated structure and added bootstrap

---
+++ src/Pages/HomePage/index.jsx
@@ -0,0 +1,17 @@
+// HomePage.js
+import React from 'react';
+import Navbar from './Navbar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <div>Hassan</div>
+        <div>Mehrab</div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
